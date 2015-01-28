418ce22f4b87155f5fb1821eb720550526714156
ReallyBigData <jss74@duke.edu>
2015-01-28 15:28:12 -0500

    Class 1 commit

---
+++ 1_data_types_and_lists.py
@@ -0,0 +1,6 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+main_dir = "/Users/jseidenfeld/Desktop/Data"
+git_dir = "/Users/jseidenfeld/Documents/School/Classes/Big Data for Energy 590/BigData590GitHubJS/PubPol590GitHubJS"
